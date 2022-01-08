c495751d5f0e53debc27240539c5b1e78c5da282
Caroline Peri <caroline.anne.peri@gmail.com>
2022-01-08 18:04:05 -0500

    fix formatting

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,27 +1,27 @@
 class ItemsController < ApplicationController
   helper ItemHelper
   before_action :find_item, only: [:show, :edit, :update]
-  
+
   def index
     @items = Item.all
   end
-  
+
   def new
-    @item = Item.new 
-  end 
+    @item = Item.new
+  end
 
   def create
     @item = Item.new(item_params)
 
     respond_to do |format|
-      if @item.save 
+      if @item.save
         format.html { redirect_to items_path, notice: "#{@item.name} item successfully created." }
       else
         format.html { render :new, status: :unprocessable_entity }
       end
     end
   end
-  
+
   def show
   end
 
@@ -37,13 +37,13 @@ class ItemsController < ApplicationController
       end
     end
   end
-  
+
   private
   def item_params
     params.require(:item).permit(:name, :quantity, :upc, :description, :price, :weight)
   end
-  
+
   def find_item
     @item = Item.find(params[:id])
   end
-end 
+end

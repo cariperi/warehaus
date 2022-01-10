3658ca8a3197de886a736f8b24103e494a468eff
Caroline Peri <caroline.anne.peri@gmail.com>
2022-01-10 16:18:08 -0500

    transition database from sqlite to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2022_01_08_001820) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "items", force: :cascade do |t|
     t.string "name", null: false
     t.integer "quantity", null: false
